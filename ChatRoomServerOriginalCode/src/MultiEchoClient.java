9cb56d4fcf599b176f149f8727e39d76c8793041
User <jamesrmclean88@gmail.com>
2021-12-28 22:12:16 -0400

    User validation checks names properly, duplicate names break loop

--- ChatRoomServerOriginalCode/src/MultiEchoClient.java
+++ ChatRoomServerOriginalCode/src/MultiEchoClient.java
@@ -1,8 +1,6 @@
-import com.sun.org.apache.xpath.internal.operations.Bool;
-
 import java.io.*;
 import java.net.*;
-public class MultiEchoClient{
+public class MultiEchoClient {
 	private static final int PORT = 1234;
 	private static Socket link;
 	private static BufferedReader in;
@@ -10,7 +8,6 @@ public class MultiEchoClient{
 	private static BufferedReader kbd;
 	private static String userName;
 
-	//
 	public static void main(String[] args) throws Exception{
 		try{
 			link = new Socket("127.0.0.1", PORT);
@@ -18,24 +15,33 @@ public class MultiEchoClient{
 			out = new PrintWriter(link.getOutputStream(), true);
 			kbd = new BufferedReader(new InputStreamReader(System.in));
 			String message, response;
+			boolean userAccepted = false;
 
 			//Takes user input for username
-			System.out.println("Enter Username: ");
-			userName = kbd.readLine();
-			out.println(userName);
-//					response = in.readLine();
-			System.out.println("You are connected as: " + userName);
 			do {
+				String tempName;
+				System.out.println("Enter a username (BYE to quit):");
+				tempName = kbd.readLine();
+				out.println(tempName);
+				response = in.readLine();
+//				System.out.println(response);
+				if(response.equals("USERNAME OK")){
+					userAccepted = true;
+				}else if(response.equals("USERNAME ALREADY IN CLIENTLIST")){
+					userAccepted = false;
+				}
+			}while(!userAccepted);
 
+			System.out.println("You are connected as: " + userName);
 
+			do {
 				System.out.println("Enter message (BYE to quit)");
 				message = kbd.readLine();
 				out.println(userName + ": " + message);
-//				response = in.readLine();
-//				System.out.println(response);
 
 			}while (!message.equals("BYE"));
 		}
+
 		catch(UnknownHostException e){System.exit(1);}
 		catch(IOException e){System.exit(1);}
 		finally{
@@ -48,6 +54,18 @@ public class MultiEchoClient{
 			catch(IOException e){System.exit(1);}
 		}
 	}//end main
+
+
+
+	public boolean messageStartsWith(String message, String startsWith){
+		int start = 0;
+		int end = (startsWith.length()-1);
+		if(message.substring(start, end).equalsIgnoreCase(startsWith)){
+			return true;
+		}else{
+			return false;
+		}
+	}
 }//end class MultiEchoClient
 	
 	
--- ChatRoomServerOriginalCode/src/MultiEchoServer.java
+++ ChatRoomServerOriginalCode/src/MultiEchoServer.java
@@ -1,127 +1,194 @@
+import com.sun.security.ntlm.Client;
+
 import java.io.*;
 import java.net.*;
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
-public class MultiEchoServer{
+public class MultiEchoServer {
 	//declare the ServerSocket variable and the port number for the server(constant)
 	private static ServerSocket serverSock;
 	private static final int PORT = 1234;
-	public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+	public static Set<ClientHandler> clientHandlers = new HashSet<>();
+	public static Set<String> clientUserNames = new HashSet<>();
 
-
-	//The main method will create the ServerSocket object and listens to inputs
-	//from multiple clients
-
-	public void run() throws IOException{
-		try{
+	//The main method will create the ServerSocket object and listens to inputs from multiple clients
+	public void run(){
+		try {
 			serverSock = new ServerSocket(PORT);
-		}
-		catch(IOException e){
-			System.out.println("[CAN'T LISTEN ON PORT" + PORT +"]");
+		} catch (IOException e) {
+			System.out.println("[CAN'T LISTEN ON PORT" + PORT + "]");
 			System.exit(1);
 		}
-		do{
-			Socket client = null;
+		do {
+			Socket client;
 			System.out.println("[LISTENING FOR NEW CONNECTIONS...]");
-			try{
+			try {
 				client = serverSock.accept();
 				System.out.println("[NEW CLIENT ACCEPTED]");
-				ClientHandler handler = new ClientHandler(client);
+				ClientHandler handler = new ClientHandler(client, this);
 				addHandler(handler);
 				handler.start();
-			}
-			catch(IOException e){
+
+				//Prints a list of active threads in clientHandlers list
+				System.out.println(getHandlers().toString());
+			} catch (IOException e) {
 				System.out.println("[ACCEPT FAILED]");
 				System.exit(1);
 			}
-			System.out.println("[CONNECTION SUCCESSFUL]");
 			System.out.println("[LISTENING FOR INPUT...]");
-
-
 
 		} while (true);
 
 	}//end run
-	public static void addHandler(ClientHandler clientHandler){
+
+	//Adds handler to HashSet clientHandlers
+	void addHandler(ClientHandler clientHandler) {
 		clientHandlers.add(clientHandler);
 	}
 
+	//Removes handler from Hashset clientHandlers
+	void removeHandler(ClientHandler clientHandler) {clientHandlers.remove(clientHandler); }
+
+	//Adds user to HashSet userNames
+	void addUserName(String user) {clientUserNames.add(user); }
+
+	//Removes user from HashSet userNames
+	void removeUserName(String user){clientUserNames.remove(user); }
+
+	//Returns a list of current client usernames from HashSet clientUserNames
+	public Set<String> getClientUserNames(){
+		return clientUserNames;
+	}
+
 	//Gets list of ClientHandlers
-	public static ArrayList<ClientHandler> getHandlers(){
+	public Set<ClientHandler> getHandlers() {
 		return clientHandlers;
 	}
 
-	public static void broadcastMessage(String broadcastMessage, ClientHandler activeHandler){
-		for(ClientHandler clientHandler: clientHandlers){
-			if(!clientHandler.getUserName().equals(activeHandler)){
-				clientHandler.sendMessage(broadcastMessage);
+	//Sends message from one user/client to other active users/clients
+	void broadcastMessage(String broadcastMessage, ClientHandler activeClient){
+		for (ClientHandler client: clientHandlers){
+			if(client != activeClient){
+				activeClient.sendMessage(broadcastMessage);
 			}
 		}
 	}
 	//This is a support class that extends Thread, runs the client thread
-//and sends and receives messages
+	//and sends and receives messages
 
 	public class ClientHandler extends Thread{
 
+		//Local variables for ClientHandler class
+		private MultiEchoServer chatroomServer;
 		private Socket clientSocket;
 		private BufferedReader in;
 		private PrintWriter out;
 		private String userName;
+		private boolean nameAccepted = false;
 
-		public ClientHandler(Socket socket){
-			clientSocket = socket;
-			try{
-				in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-				out = new PrintWriter(clientSocket.getOutputStream(), true);
-			}
-			catch(IOException e){
-				e.printStackTrace();
-			}
+		//Constructor for ClientHandler class
+		public ClientHandler(Socket clientSocket, MultiEchoServer chatroomServer){
+			this.clientSocket = clientSocket;
+			this.chatroomServer = chatroomServer;
 		}
-		public void run(){
-			userName = null;
+
+		//Method to run ClientHandler thread
+		public void run() {
+
+			//Local variables for run method
+			String tempName;
+			boolean contains = false;
+
+			//Initializes input and output stream for ClientHandler
 			try {
-				userName = in.readLine();
+					in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+					out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+				//Takes user input for username, checks if username is valid, and sets ClientHandler username
+				do{
+					tempName = in.readLine();
+					if (tempName == null) {
+						return;
+					}
+
+					//Checks to see if list of handlers contains input username
+					if (chatroomServer.getClientUserNames().contains(tempName)) {
+						out.println("USERNAME ALREADY IN CLIENTLIST");
+						contains = true;
+					}
+
+					//If username is not in userNameList
+					if(!contains){
+						nameAccepted = true;
+						out.println("USERNAME OK");
+					}
+
+				}while (!nameAccepted);
+
+				//Sets username and adds ClientHandler clientHandlers list
+				setUserName(tempName);
+				addUserName(tempName);
+				addHandler(this);
+				System.out.println("[" + userName + " CONNECTED TO SERVER]");
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			out.println(userName);
-			System.out.println("[" + userName + " CONNECTED TO SERVER]");
 
-			try{
+			try {
 				String received;
-				do{
+				do {
 					received = in.readLine();
-					sendMessage(received);
-					System.out.println(received);
-					out.println(received);
-
+					String message = (received);
+					chatroomServer.broadcastMessage(message, this);
+					System.out.println(message);
 				} while (!received.equals("BYE"));
-			}
-			catch(IOException e){
-				System.out.println("[USER HAS REQUESTED TO DISCONNECT FROM SERVER]");
 
-			}
-			finally{
-				try{
-					if(clientSocket!=null){
-						System.out.println("[CLOSING CONNECTION TO " + userName + "]");
-						clientSocket.close();
+				//Outputs disconnect message to server
+				if(received.equals("BYE")) {
+					System.out.println("[USER HAS REQUESTED TO DISCONNECT FROM SERVER]");
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+
+			//
+			}finally{
+					try {
+						if (clientSocket != null) {
+							System.out.println("[CLOSING CONNECTION TO " + userName + "]");
+							removeHandler(this);
+							removeUserName(this.getUserName());
+							clientSocket.close();
+						}
+
+					} catch (IOException e) {
+						e.printStackTrace();
 					}
 				}
-				catch(IOException e){
-					e.printStackTrace();
-				}
-			}
-		}//end run
-		public void sendMessage(String message){
+			}//end run
+
+		//Sends a message from the client to the server using PrintWriter
+		void sendMessage(String message){
 			out.println(message);
 		}
 
+		//Gets client userName from list of ClientHandler
 		public String getUserName(){
 			return userName;
 		}
+
+		//Sets clients userName in ClientHandler
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
 	}//end ClientHandler class
 
+	//Main method for MultiEchoServer class
+	public static void main(String[] args) {
+		MultiEchoServer chatroomServer = new MultiEchoServer();
+		chatroomServer.run();
+	}
 }//end class MultiEchoServer
 
